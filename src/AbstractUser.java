a037e689a5649960b4a49e0880fa4f9f64897f9f
SonaSaha <55319268+SonaSaha@users.noreply.github.com>
2020-08-06 13:10:26 +0400

    origin

---
+++ src/AbstractUser.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class AbstractUser implements IUser {
+    /**
+     * array which Contains materials of user
+     */
+ protected ArrayList<MaterialStack> materials = new ArrayList<>();
+
+    /**
+     *
+     * @return materials of user
+     */
+    @Override
+    public ArrayList<MaterialStack> getUserMaterials() {
+        return materials;
+    }
+}
---
+++ src/IUser.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public interface IUser {
+    /**
+     *
+     * @return array of user materials
+     */
+    ArrayList<MaterialStack> getUserMaterials();
+}
